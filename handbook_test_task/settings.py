cac579266288a57edeea9b104e4f58fed395ec4b
tchocorpse <theunknownperson@mail.ru>
2021-06-23 11:57:03 +0300

    refactoring fix

--- handbook_test_task/settings.py
+++ handbook_test_task/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'komtek_task.urls'
+ROOT_URLCONF = 'handbook_test_task.urls'
 
 TEMPLATES = [
     {
